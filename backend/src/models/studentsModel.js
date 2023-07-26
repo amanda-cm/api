122aba7019482b827e182df92e186dbdea7b41c9
Amanda <amanda.colinm@gmail.com>
2023-07-26 18:30:14 -0300

    inserção no banco de dados

--- backend/src/models/studentsModel.js
+++ backend/src/models/studentsModel.js
@@ -2,7 +2,7 @@ const connection = require('./connection');
 
 const getAll = async () => { //busca tuso o que tem no banco de dados e cria uma função assincrona
     const students = await connection.execute('SELECT * FROM students'); // aluno vai receber todos os estudantes que tem no banco, await espera o termino da execução 
-    return students;
+    return students[0]; //retornar somente o array da posição 0 o outro tem informações que não são do banco
 };
 
 module.exports = { //exporta um objeto e dentro desse objeto vai ir as funções acima
